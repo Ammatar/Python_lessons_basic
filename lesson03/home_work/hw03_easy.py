f72acda7cc3c7cb29f3c0fcb175b3054e01ed594
Ammatar <42938122+Ammatar@users.noreply.github.com>
2018-09-13 13:09:26 +0300

    Update hw03_easy.py

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -5,7 +5,17 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    pass
+    number_temp1 = number*(10**ndigits)
+    number_temp_ostatok = number_temp1 % 1
+    #print(number_temp_ostatok)
+    if number_temp_ostatok > 0.4:
+        number_add = 1 - number_temp_ostatok
+        round_number = number + (number_add/(10**ndigits))
+    else:
+        number_add = number_temp_ostatok - 1
+        round_number = number - (number_add/(10**ndigits))
+    #print(round_number)
+    return round_number
 
 
 print(my_round(2.1234567, 5))
@@ -20,7 +30,20 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
+    ticket_number_map = list(map(int, str(ticket_number)))
+    try:
+        ticket_part_1 = ticket_number_map[0] + ticket_number_map[1] + ticket_number_map[2]
+    except IndexError:
+        pass
+    try:
+        ticket_part_2 = ticket_number_map[3]+ ticket_number_map[4]+ticket_number_map[5]
+    except IndexError:
+        pass
+    try:
+        result = ticket_part_1 == ticket_part_2
+        return (result)
+    except:
+        pass
 
 
 print(lucky_ticket(123006))
